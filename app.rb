fe1d14d047861c4ec305c4aa332908f7ca99e308
Joseph Sieger <jsieger44@gmail.com>
2023-12-28 22:05:00 +0000

    homepage working

--- app.rb
+++ app.rb
@@ -1,3 +1,24 @@
 require "sinatra"
 require "sinatra/reloader"
 require "http"
+
+get("/") do
+
+    # build the API url, including the API key in the query string
+    api_url = "http://api.coinlayer.com/api/live?access_key=#{ENV["COINLAYER_KEY"]}"
+  
+    # use HTTP.get to retrieve the API information
+    raw_data = HTTP.get(api_url)
+  
+    # convert the raw request to a string
+    raw_data_string = raw_data.to_s
+  
+    # convert the string to JSON
+    parsed_data = JSON.parse(raw_data_string)
+  
+    # get the symbols from the JSON
+    @symbols = parsed_data["rates"]
+  
+    # render a view template where I show the symbols
+    erb(:homepage)
+  end
